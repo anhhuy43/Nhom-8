f7c22612563933dd81d0dfb5a6753dcb53a3403c
Nguyen Hoang Anh <anhnhtech@gmail.com>
2025-03-11 09:49:23 +0700

    add menu page

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
-// Cấu hình thư mục chứa file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Định tuyến trang chủ
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Khởi động server
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'menu.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
